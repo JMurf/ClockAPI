04dac1ce49b99467ca4a4bb2940f0a2e4670e81e
Jonathan <jonathanmurfey@hotmail.com>
2019-05-18 20:00:07 -0500

    added files

---
+++ TimeClock/Models/ReqClock.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using TimeClock.DTOs;
+
+namespace TimeClock.Models
+{
+    public class Request
+    {
+        public delegate void DataProc(PunchClockDTO tr);
+
+        public string Pass { get; set; }
+        public int TaskNo { get; set; }
+        public bool Result { get; set; }
+        public string InterfaceName { get; set; }
+        [JsonIgnore]
+        public ManualResetEvent mre { get; set; }
+
+        [JsonIgnore]
+        public DataProc ProcessData { get; set; }
+
+        public Request()
+        {
+            TaskNo = (new Random()).Next(Int32.MaxValue);
+        }
+        public override string ToString()
+        {
+            return "Task No: " + TaskNo + "   InterfaceName: " + InterfaceName;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            Request objAsReq = obj as Request;
+            if (objAsReq == null) return false;
+            else return Equals(objAsReq);
+        }
+        public bool Equals(Request other)
+        {
+            if (other == null) return false;
+            return (this.TaskNo.Equals(other.TaskNo));
+        }
+        public override int GetHashCode()
+        {
+            return TaskNo;
+        }
+    }
+}
---
+++ TimeClock/Models/ReqFindRecords.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Web.Hosting;
+using TimeClock.DTOs;
+
+namespace TimeClock.Models
+{
+    public class RequestFindRecords : Request
+    {
+        [JsonIgnore]
+        private static readonly log4net.ILog log =
+            log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        /* delegate */
+        public void ProcessFindRecords(PunchClockDTO data)
+        {
+            log.Info(data);
+            PunchList = new List<PunchRecordDTO>();
+            PunchRecordDTO pr = new PunchRecordDTO();
+            pr.EmployeeId = "12345A";
+            PunchList.Add(pr);
+
+            pr = new PunchRecordDTO();
+            pr.EmployeeId = "54321B";
+            PunchList.Add(pr);
+        }
+
+        public RequestFindRecords()
+        {
+            log4net.Config.XmlConfigurator.Configure(new FileInfo(HostingEnvironment.MapPath("~/log4net.config")));
+            ProcessData = new DataProc(ProcessFindRecords);
+        }
+        public string personId { get; set; }
+        public string length { get; set; }
+        public string index { get; set; }
+        public string startTime { get; set; }
+        public string endTime { get; set; }
+        public List<PunchRecordDTO> PunchList { get; set; }
+
+        public List<PunchRecordDTO> getData()
+        {
+            List<PunchRecordDTO> list = new List<PunchRecordDTO>();
+
+
+            return list;
+        }
+        public void fill()
+        {
+            Pass = "123456";
+            InterfaceName = "findRecords";
+            personId = "-1";
+            startTime = "0";
+            length = "-1";
+            index = "0";
+            endTime = "2019-05-06 01:00:05";
+            Result = true;
+        }
+    }
+}
